d943994e72239fe72fcbbe2a86cc4dccaf169fa7
Naskapie <naskapie@gmx.net>
2020-05-11 18:44:37 +0200

    Update UI

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight = 80.0;
+const activeCardColour = Color(0xFF1D1E33);
+const bottomContainerColour = Color(0xFFEB1555);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
 }
 
 class _InputPageState extends State<InputPage> {
+  // Gender selectedGender;
+  int height = 180;
+  int weight = 60;
+  int age = 20;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,19 +25,40 @@ class _InputPageState extends State<InputPage> {
         Expanded(
           child: Row(
             children: [
-              Expanded(child: ReusableCard()),
-              Expanded(child: ReusableCard()),
+              Expanded(
+                  child: ReusableCard(
+                colour: activeCardColour,
+              )),
+              Expanded(
+                  child: ReusableCard(
+                colour: activeCardColour,
+              )),
             ],
           ),
         ),
-        Expanded(child: ReusableCard()),
+        Expanded(
+            child: ReusableCard(
+          colour: activeCardColour,
+        )),
         Expanded(
           child: Row(
             children: [
-              Expanded(child: ReusableCard()),
-              Expanded(child: ReusableCard()),
+              Expanded(
+                  child: ReusableCard(
+                colour: activeCardColour,
+              )),
+              Expanded(
+                  child: ReusableCard(
+                colour: activeCardColour,
+              )),
             ],
           ),
+        ),
+        Container(
+          color: bottomContainerColour,
+          margin: EdgeInsets.only(top: 10.0),
+          width: double.infinity,
+          height: bottomContainerHeight,
         )
       ]),
     );

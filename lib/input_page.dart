6d87833c0a7d6dc960cce0c304f726d137de5ff1
Naskapie <naskapie@gmx.net>
2020-05-11 20:03:40 +0200

    Bugfix

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'icon_content.dart';
+import 'reusable_card.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColour = Color(0xFF1D1E33);
@@ -28,10 +32,18 @@ class _InputPageState extends State<InputPage> {
               Expanded(
                   child: ReusableCard(
                 colour: activeCardColour,
+                cardChild: IconContent(
+                  icon: FontAwesomeIcons.mars,
+                  label: 'MEN',
+                ),
               )),
               Expanded(
                   child: ReusableCard(
                 colour: activeCardColour,
+                cardChild: IconContent(
+                  icon: FontAwesomeIcons.venus,
+                  label: 'FEMALE',
+                ),
               )),
             ],
           ),
@@ -46,10 +58,18 @@ class _InputPageState extends State<InputPage> {
               Expanded(
                   child: ReusableCard(
                 colour: activeCardColour,
+                cardChild: IconContent(
+                  icon: FontAwesomeIcons.venus,
+                  label: 'WEIGHT',
+                ),
               )),
               Expanded(
                   child: ReusableCard(
                 colour: activeCardColour,
+                cardChild: IconContent(
+                  icon: FontAwesomeIcons.venus,
+                  label: 'AGE',
+                ),
               )),
             ],
           ),
@@ -61,35 +81,6 @@ class _InputPageState extends State<InputPage> {
           height: bottomContainerHeight,
         )
       ]),
-    );
-  }
-}
-
-class IconContent extends StatelessWidget {
-  IconContent({@required this.icon, this.label});
-
-  final IconData icon;
-  final label;
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-  }
-}
-
-class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.colour, this.cardChild});
-
-  final Color colour;
-  final Widget cardChild;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-          color: colour, borderRadius: BorderRadius.circular(10.0)),
     );
   }
 }
